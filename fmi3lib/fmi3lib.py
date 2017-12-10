a8bbad87afb797594301481f023699f325222cf6
Fenner Macrae <fennermacrae@gmail.com>
2017-12-10 15:11:56 -0800

    Added empty workspace function

--- fmi3lib/fmi3lib.py
+++ fmi3lib/fmi3lib.py
@@ -1,6 +1,16 @@
 """Personal library code for i3 scripting"""
 import i3
 
+def empty_workspace():
+    """Return the index of the first empty workspace"""
+    workspaces = i3.get_workspaces()
+    for i in range(1, len(workspaces)):
+        if i != workspaces[i]['num'] - 1:
+            return i + 1
+    return i + 2
+
+
+
 def focused_workspace():
     """Return the number of the workspace that is currently in focus"""
     for workspace in i3.get_workspaces():
